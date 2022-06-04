d716e309a6e2d56b37dbe1eca2493d49cf5fb20e
Dark Tornado <dt3141592@naver.com>
2022-06-05 04:06:58 +0900

    init

---
+++ java/com/darktornado/simplereplybot/Bot.java
@@ -0,0 +1,4 @@
+package com.darktornado.simplereplybot;
+
+public class Bot {
+}
---
+++ java/com/darktornado/simplereplybot/KakaoTalkListener.java
@@ -0,0 +1,6 @@
+package com.darktornado.simplereplybot;
+
+import android.service.notification.NotificationListenerService;
+
+public class KakaoTalkListener extends NotificationListenerService {
+}
