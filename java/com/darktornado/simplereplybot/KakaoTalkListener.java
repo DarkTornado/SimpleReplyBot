170ff5f2f59caa0474130e06afad460c9476b0a1
Dark Tornado <dt3141592@naver.com>
2022-06-07 07:22:10 +0900

    알림 파싱

--- java/com/darktornado/simplereplybot/KakaoTalkListener.java
+++ java/com/darktornado/simplereplybot/KakaoTalkListener.java
@@ -1,6 +1,33 @@
 package com.darktornado.simplereplybot;
 
+import android.app.Notification;
+import android.os.Bundle;
 import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
 
 public class KakaoTalkListener extends NotificationListenerService {
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        super.onNotificationPosted(sbn);
+        if (!Bot.readBoolean(this, "botOn")) return;
+        final String packageName = sbn.getPackageName();
+        try {
+            Notification.Action[] actions = sbn.getNotification().actions;
+            if (actions.length == 0) return;
+
+            Bundle bundle = sbn.getNotification().extras;
+            String msg = bundle.get("android.text").toString();
+            String sender = bundle.getString("android.title");
+            String room = bundle.getString("android.subText");
+            boolean isGroupChat = room != null;
+            if (room == null) room = sender;
+
+        } catch (Exception e) {
+            Log.i(Bot.TAG, e.toString());
+        }
+        //toast(a);
+    }
+
 }
